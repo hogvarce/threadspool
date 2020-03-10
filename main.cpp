71aaf2d43e750e6e29a5e2aa6a4d058d4f221de8
hogvarce <hogvarce@gmail.com>
2020-03-11 01:11:59 +0300

    proggress

--- main.cpp
+++ main.cpp
@@ -3,11 +3,15 @@
 #include <vector>
 #include <functional>
 #include <mutex>
+#include <condition_variable>
+#include <chrono>
+#include <atomic>
 
 class ThreadPoll {
     public:
         ThreadPoll() {
             stop = false;
+            notified = false;
             unsigned int coreCount = std::thread::hardware_concurrency();
             for (int i = 0; i < coreCount; ++i) {
                 threads.push_back(std::thread(&ThreadPoll::runner, this));
@@ -21,26 +25,50 @@ class ThreadPoll {
         void addTask(std::function<void()> Func) {
             std::unique_lock<std::mutex> lck (mtx);
             tasks.push_back(Func);
+            notified = true;
+            cond_var.notify_all();
         }
     private:
         std::vector<std::thread> threads;
         std::vector<std::function<void()>> tasks;
+        std::condition_variable cond_var;
         std::mutex mtx;
+        std::atomic_bool notified;
         bool stop;
         void runner() {
             while(!stop) {
-                std::unique_lock<std::mutex> lck (mtx);
-                if (!tasks.empty())
-                    tasks[0]();
+                std::function<void()> func;
+                {
+                    std::unique_lock<std::mutex> lck (mtx);
+                    cond_var.wait(lck, [this]{return notified.load();});
+                    if (tasks.empty()) continue;
+                    func = tasks.back();
+                    tasks.pop_back();
+                }
+                func();
             }
         }
 };
 
 void test() {
-    std::cout << "I`m thread" << std::endl;
+    std::cout << std::this_thread::get_id() << std::endl;
 }
+
+void Print() {
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    for (auto i = 0; i < 5; ++i) {
+        for (auto j = 0; j < 10; j++) {
+            std::cout << std::this_thread::get_id();
+        }
+        std::cout << std::endl;
+    }
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+}
+
 int main() {
     ThreadPoll pool;
+    test();
+    pool.addTask(Print);
     pool.addTask(test);
-    std::cout << "main ends" << std::endl;
+    std::cout << std::this_thread::get_id() << std::endl;
 }
